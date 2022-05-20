647e0ce6759d0a643981ace996d53f67080baa56
Wooseok Jang <mmyu2090@gmail.com>
2022-05-20 21:54:58 +0900

    bug fixed

--- Assets/Scripts/MainUiEvent.cs
+++ Assets/Scripts/MainUiEvent.cs
@@ -5,31 +5,26 @@ using UnityEngine.UI;
 
 public class MainUiEvent : MonoBehaviour
 {
-    [SerializeField] GameObject newProObj;
-    [SerializeField] GameObject oldProObj;
-    RectTransform rectNewProObj;
-    RectTransform rectOldProObj;
+    [SerializeField] GameObject ProObj;
+    RectTransform rectProObj;
 
-    Vector2 NewProObjDes = Vector2.zero;
-    Vector2 OldProObjDes = Vector2.zero;
+    Vector2 ProObjDes;
     Vector2 velocity = Vector2.zero;
     float smoothTime = 0.3f;
     void Start()
     {
-        rectNewProObj = newProObj.GetComponent<RectTransform>();
-        rectOldProObj = oldProObj.GetComponent<RectTransform>();
-
+        rectProObj = ProObj.GetComponent<RectTransform>();
+        ProObjDes = rectProObj.anchoredPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rectNewProObj.anchoredPosition = Vector2.SmoothDamp(rectNewProObj.anchoredPosition, NewProObjDes, ref velocity, smoothTime);
-        rectOldProObj.anchoredPosition = Vector2.SmoothDamp(rectOldProObj.anchoredPosition, OldProObjDes, ref velocity, smoothTime);
+        rectProObj.anchoredPosition = Vector2.SmoothDamp(rectProObj.anchoredPosition, ProObjDes, ref velocity, smoothTime);
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            NewProObjDes = new Vector2(-Screen.width, 0);
+            ProObjDes = new Vector2(-Screen.width, 0);
             //OldProObjDes = new Vector2(1000, 0);
         }
     }
