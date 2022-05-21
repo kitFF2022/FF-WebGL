02c7d9df424bd58c3b6bcc42bc420136fa529c65
Karadaikon <86056888+Karadaikon@users.noreply.github.com>
2022-05-21 23:40:55 +0900

    Scene3 Added
    Scene3 Added

---
+++ Assets/Scripts/Object/GetObjectInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetObjectInfo : MonoBehaviour
+{
+    public static GetObjectInfo Instance { get; private set; }
+
+    //private List<GameObject> gameObjectList;
+    private GameObject currentGameObject;
+    RaycastHit raycastHit;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObjectList = new List<GameObject>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetCurrentGameObject() {
+        raycastHit = Mouse3D.GetMouseClickedObjectHit();
+        currentGameObject = raycastHit.transform.gameObject;
+        Debug.Log(currentGameObject);
+        //gameObjectList.Add(raycastHit.transform.gameObject);
+        return currentGameObject;
+    }
+}
---
+++ Assets/Scripts/Object/ObjectCollision.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollision : MonoBehaviour
+{
+    public static ObjectCollision Instance { get; private set; }
+
+    [SerializeField] private Material[] material;
+    Renderer rend;
+    public int collisionOn;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionOn = 0;
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[collisionOn];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.sharedMaterial = material[collisionOn];
+
+        if(Input.GetKeyDown(KeyCode.LeftShift)) {
+            collisionOn = 1;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift)) {
+            collisionOn = 0;
+        }
+
+
+        
+    }
+    void OnTriggerStay(Collider other) {
+        if (other.transform.tag == "Object") {
+            //Debug.Log("if");
+
+            collisionOn = 1;
+        } 
+    }
+    void OnTriggerExit(Collider other) 
+	{
+		if (other.transform.tag == "Object") {
+            //Debug.Log("if");
+
+            collisionOn = 0;
+        } 
+	}
+
+}
